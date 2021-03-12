c19c30e790899fa1167276a0e9e271988679fd78
simon921 <simon921@gmail.com>
2021-03-12 18:42:02 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # coding=gbk
+import difflib
 
 f = open("title.txt", mode="r")
 a = f.readlines()
@@ -6,13 +7,15 @@ a = f.readlines()
 b = '国内新闻：北京疾控：接种新冠疫苗后不得带离按压棉签'
 c = '国内新闻：北京市属公园11日至17日免费开放，8日开始预约'
 
-import difflib
 
 def string_similar(s1, s2):
-    return difflib.SequenceMatcher(lambda x:x == "\n", s1, s2).quick_ratio()
+    return difflib.SequenceMatcher(lambda x: x == "\n", s1, s2).quick_ratio()
+
+
 m = 1
+
 for i in a:
-    print(m-1)
-    print(a[m-1], a[m],string_similar(a[m-1], a[m]))
+    print(m - 1)
+    print(a[m - 1], a[m], string_similar(a[m - 1], a[m]))
     m += 1
     print(m)
