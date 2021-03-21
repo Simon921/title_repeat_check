8b554c0c8c21655cb7e909a0c736b99cd252d3bc
Simon <simon921@gmail.com>
2021-03-21 10:56:59 +0800

    update for循环 同一list取两个名字 遍历比较相似度

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 # coding=gbk
-
-f = open("title.txt", mode="r")
-a = f.readlines()
-
-b = '国内新闻：北京疾控：接种新冠疫苗后不得带离按压棉签'
-c = '国内新闻：北京市属公园11日至17日免费开放，8日开始预约'
-
 import difflib
 
+f = open("title.txt", mode="r", encoding='gbk')
+a = f.readlines()
+f.close()
+q = open("title.txt", mode="r", encoding='gbk')
+b = q.readlines()
+q.close()
+
+
 def string_similar(s1, s2):
-    return difflib.SequenceMatcher(lambda x:x == "\n", s1, s2).quick_ratio()
-m = 1
+    return difflib.SequenceMatcher(lambda x: x == "\n", s1, s2).quick_ratio()
+
+
 for i in a:
-    print(m-1)
-    print(a[m-1], a[m],string_similar(a[m-1], a[m]))
-    m += 1
-    print(m)
+    for k in b:
+        print(i, k, string_similar(i, k))
+
+
