373745bfc67a425fe669a22d0492a88c22699e83
simon921 <simon921@gmail.com>
2021-03-23 19:00:03 +0800

    update with list

--- main.py
+++ main.py
@@ -10,15 +10,22 @@ q.close()
 
 
 def string_similar(s1, s2):
-    return difflib.SequenceMatcher(lambda x: x == "\n", s1, s2).quick_ratio()
+    return difflib.SequenceMatcher(lambda x: x == '\n国内新闻国际新闻', s1, s2).quick_ratio()
 
+
+with open('test.txt', "w") as f:
+    f.write('')
+
+r = []
 
 for i in a:
     for k in b:
-        if 1 > string_similar(i, k) > 0.5:
-            # print(i, k, string_similar(i, k))
-            samestr = i + k + str(string_similar(i, k)) + "\n\n"
-            with open("test.txt", "a+") as f:
-                f.write(samestr)
+        samerate = i + k + str(string_similar(i, k)) + "\n\n"
+        if 1 > string_similar(i, k) > 0.55:
+            r.append(samerate)
+            print(i, k)
 
 
+with open('test.txt', 'a') as f:
+    for i in r:
+        f.write(i)
